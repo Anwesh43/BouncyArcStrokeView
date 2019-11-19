bd0876be0be9f71dc8833c8b315159228bfa745c
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 13:55:14 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbouncyarcstrokeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbouncyarcstrokeview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbouncyarcstrokeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bouncyarcstrokeview.BouncyArcStrokeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BouncyArcStrokeView.create(this)
     }
 }
