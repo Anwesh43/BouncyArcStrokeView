6c8f2aa4234f6b68b84bfee901643e0f579e3a61
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 13:21:08 +0530

    adding BASNode class

--- bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
+++ bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
@@ -114,4 +114,46 @@ class BouncyArcStrokeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BASNode(var i : Int, val state : State = State()) {
+
+        private var prev : BASNode? = null
+        private var next : BASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBASNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BASNode {
+            var curr : BASNode?= prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
