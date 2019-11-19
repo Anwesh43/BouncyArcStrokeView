0a40706a3e120c43ea4bf0019bc820936fd35a5b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 12:45:02 +0530

    adding constants and libraries

--- bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
+++ bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bouncyarcstrokeview
 /**
  * Created by anweshmishra on 19/11/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
