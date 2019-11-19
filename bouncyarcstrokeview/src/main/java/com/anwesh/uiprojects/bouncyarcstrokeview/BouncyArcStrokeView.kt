01d0ee172f3b1b8be543f4b2b31dde8cf161fcba
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 13:26:06 +0530

    adding BouncyArcStroke class

--- bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
+++ bouncyarcstrokeview/src/main/java/com/anwesh/uiprojects/bouncyarcstrokeview/BouncyArcStrokeView.kt
@@ -156,4 +156,27 @@ class BouncyArcStrokeView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BouncyArcStroke(var i : Int) {
+
+        private var curr : BASNode = BASNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
